749b296692759061c7bcd5976858eda267f8fbd4
SantiagoGaona <sgaonacarvajal@outlook.com>
2023-07-17 21:01:05 -0500

    feat: update desc

--- src/app/components/text/MyText.tsx
+++ src/app/components/text/MyText.tsx
@@ -1,12 +1,4 @@
 export const MyTitle = `Hi! I'm Santiago Gaona, <br /> <b>Fullstack developer</b> <br />
   with <b>React</b> and <b>Node</b>`;
 
-export const MyDesc = `Currently based in Bucaramanga, Colombia, I am a 23-year-old software
-  developer proficient in MERN stack and .NET. Over the past two years,
-  I've worked on diverse professional, freelance, and academic
-  projects, demonstrating my keen eye for detail and strong team
-  collaboration skills. While I've recently delved into user
-  experience enhancements, my core expertise remains in backend
-  development. Committed to staying updated with the latest technological
-  trends, I ensure adherence to industry best practices in all my
-  projects.`;
+export const MyDesc = `I am a 23-year-old software developer currently based in Bucaramanga, Colombia. I am proficient in the MERN stack and .NET. Over the past two years, I've worked on a diverse range of professional, freelance, and academic projects, demonstrating both a keen eye for detail and strong team collaboration skills. Recently, I've delved into user experience enhancements, but my core expertise remains in backend development. I am committed to staying updated with the latest technological trends and ensuring adherence to industry best practices in all of my projects.`;
