8be8de4d7ab3724e56e5398f237cb49f6ff85935
SantiagoGaona <sgaonacarvajal@outlook.com>
2023-09-11 14:36:07 -0500

    feat: change title

--- src/app/components/text/MyText.tsx
+++ src/app/components/text/MyText.tsx
@@ -1,4 +1,4 @@
 export const MyTitle = `Hi! I'm Santiago Gaona, <br /> <b>Fullstack developer</b> <br />
-  with <b>React</b> and <b>Node</b>`;
+  with <b>NextJS</b> and <b>Go</b>`;
 
 export const MyDesc = `Passionate about software engineering, proactive, and committed to continuous learning. Skilled in management, development, and analysis to meet business needs through technology. Adaptable to various work frameworks with strong teamwork and technical problem-solving abilities. Experienced in RPA and a FullStack application developer with expertise in .NET, TypeScript, Go, and Flutter, serving both web and mobile clients, as well as microservices. Accomplished in diverse professional and independent projects, incorporating various frameworks and DevOps practices, while excelling in client communication and teamwork.`;
